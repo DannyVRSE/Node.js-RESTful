2042877b195b3cb5cea3ef5863e55631e870d2ab
DannyVRSE <mshindiouma1024@gmail.com>
2024-07-26 18:43:39 +0300

    document and test root route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import env from "dotenv";
 import bodyParser from "body-parser";
+import cors from "cors";
 //import {swaggerDocs as V1SwaggerDocs} from "./v1/swagger.js";
 import swaggerDocs from "./v1/swagger.js";
 env.config();
@@ -11,10 +12,17 @@ const app=express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
-app.listen(PORT, ()=>{
+/*app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
     swaggerDocs(app, PORT);
-})
+})*/
+
+if (process.env.NODE_ENV === "test"){
+    app.listen(3000, () => console.log(`⚡️[server]: Server is running at https://localhost:3000`))
+}
+
+export default app;
